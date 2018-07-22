aa2713659004c32a5dd179aab5e3dbc6d26aa415
Yohay Kamchi <yohay@Yohays-iMac.local>
2018-07-22 13:17:08 +0300

    Adding Cordova

---
+++ frontend-cordova/src/app/components/list/list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatTableDataSource } from '@angular/material';
+
+import { Issue } from '../../issue.module';
+import { IssueService } from '../../issue.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  issues: Issue[];
+  displayedColumns = ['title', 'responsible', 'severity', 'status', 'actions'];
+  constructor(private issueService: IssueService, private router: Router) { }
+
+  ngOnInit() {
+    this.fetchIssues();
+  }
+
+  fetchIssues() {
+    this.issueService
+    .getIssues()
+    .subscribe((data: Issue[]) => {
+      this.issues = data;
+      console.log('Data requested ...');
+    });
+  }
+
+  editIssue(id) {
+    this.router.navigate([`/edit/${id}`]);
+  }
+
+  deleteIssue(id) {
+    this.issueService.deleteIssue(id).subscribe(() =>  {
+      this.fetchIssues();
+    });
+  }
+}
---
+++ frontend-cordova/src/app/issue.module.ts
@@ -0,0 +1,10 @@
+export interface Issue {
+    id: string;
+    title: string;
+    responsible: string;
+    description: string;
+    severity: string;
+    status: string;
+
+
+}
